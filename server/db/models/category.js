53d9404a2cc73c57bcd7525ef3dfcd26ba16fd92
Janel Grant <janelgr5@gmail.com>
2018-04-17 15:03:16 -0400

    category model error solved

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Category = db.define('category', {
-    name: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-        notEmpty: true
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
     }
 })
 
