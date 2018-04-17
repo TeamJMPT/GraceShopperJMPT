8112fd880bcad580c24f8c191cb7f473a641c902
Janel Grant <janelgr5@gmail.com>
2018-04-17 13:57:41 -0400

    #5 express trip route find by id

--- server/api/trips.js
+++ server/api/trips.js
@@ -1,3 +1,13 @@
 const router = require('express').Router()
 const {Trip, Category} = require('../db/models')
 module.exports = router
+
+router.get('/:id', (req, res, next) => {
+    Trip.findById({
+        where: {
+            id: id
+        }
+    })
+    .then(trip => res.send(trip))
+    .catch(next)
+})
