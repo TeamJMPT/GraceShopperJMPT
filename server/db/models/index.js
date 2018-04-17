f597d4ad9f4cacd2b4d6912d199a46e3eac2af99
Janel Grant <janelgr5@gmail.com>
2018-04-17 10:57:33 -0400

    associations_1

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
-const User = require('./user')
+const User = require('./user');
+const Category = require('./category');
+const Trip = require('./trips');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +9,8 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+ Category.hasMany(Trip);
+ Trip.hasMany(Category);
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +18,7 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
-}
+  User,
+  Category,
+  Trip
+};
