32c292ec4a5ebb6f2bd62e859619240b8b9773cd
Melissa Bellah <melissabellah@gmail.com>
2018-04-17 16:04:38 -0400

    store, react, and react-redux working for trips component. rendering trip name from db

---
+++ client/store/trips.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+//ACTION TYPES
+const GET_ALL_TRIPS = 'GET_ALL_TRIPS';
+
+
+
+//ACTION CREATORS
+export function getAllTrips(trips) {
+  return {type: GET_ALL_TRIPS, trips}
+}
+
+
+
+//THUNKS
+export const fetchAllTrips = () => {
+  return dispatch => {
+    axios.get('/api/trips')
+      .then(res => res.data)
+      .then(trips => {
+        console.log('Got the trips from the db!');
+        dispatch(getAllTrips(trips))
+      })
+      .catch(console.error);
+  }
+}
+
+//REDUCER(S)
+export function tripReducer(state = [], action) {
+  switch (action.type) {
+    case GET_ALL_TRIPS:
+      return action.trips
+    default:
+      return state
+  }
+}
