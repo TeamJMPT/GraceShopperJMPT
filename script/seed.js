0dde555f5eb4a6e8935e102889b6e3adab51947d
Tasnuva Noor <noortasnuva@gmail.com>
2018-04-18 17:21:30 -0400

    images appear in single trip and title changed

--- script/seed.js
+++ script/seed.js
@@ -27,21 +27,21 @@ async function seed () {
   ])
 
   const trips = await Promise.all([
-    Trip.create({name: 'Hogwards', location: 'England', imageUrl: './images/bf1f46dbe772627d8fc1b65d0d0f5dbd.jpg', 
+    Trip.create({name: 'Hogwards', location: 'England', imageUrl: '/images/bf1f46dbe772627d8fc1b65d0d0f5dbd.jpg', 
     price: '5000', description: "Hogwarts School of Witchcraft and Wizardry, shortened to Hogwarts, is a fictional British school of magic for students aged eleven to eighteen, and is the primary setting for the first six books in J.K. Rowling's Harry Potter series."}),
-    Trip.create({name: 'Mordor', location: 'East of Gondor', imageUrl: './images/de8c77f0ca734f0455b11889bd58a582.jpg', 
+    Trip.create({name: 'Mordor', location: 'East of Gondor', imageUrl: '/images/de8c77f0ca734f0455b11889bd58a582.jpg', 
     price: '2000', description: "J. R. R. Tolkien's fictional world of Middle-earth, Mordor was the region occupied and controlled by Sauron, in the southeast of northwestern Middle-earth to the East of Anduin, the great river."}),
-    Trip.create({name: 'Jurassic Park', location: 'Kauai', imageUrl: './images/4b68b54fca576aed0d0e0074adf55d0e.jpg', 
+    Trip.create({name: 'Jurassic Park', location: 'Kauai', imageUrl: '/images/4b68b54fca576aed0d0e0074adf55d0e.jpg', 
     price: '3500', description: 'Jurassic Park is an American science fiction media franchise centered on a disastrous attempt to create a theme park of cloned dinosaurs who escape confinement and terrorize the human characters.'}),
-    Trip.create({name: 'Narnia', location: 'United Kingdom', imageUrl: './images/c8c0aadbe70397dc6c43b362081e7e8f.jpg', 
+    Trip.create({name: 'Narnia', location: 'United Kingdom', imageUrl: '/images/c8c0aadbe70397dc6c43b362081e7e8f.jpg', 
     price: '5000', description: " C. S. Lewis's a fantasy world of magic, mythical beasts, and talking animals, the series narrates the adventures of various children who play central roles in the unfolding history of that world."}),
-    Trip.create({name: 'Tatooine', location: 'Outer Rim Galaxy', imageUrl: './images/829e3861880180e8df3270be118ec368.jpg', 
+    Trip.create({name: 'Tatooine', location: 'Outer Rim Galaxy', imageUrl: '/images/829e3861880180e8df3270be118ec368.jpg', 
     price: '2000', description: 'Tatooine is a fictional desert planet that appears in the Star Wars space opera franchise. It is beige-coloured and is depicted as a remote, desolate world orbiting a pair of binary stars, and inhabited by human settlers and a variety of other life forms.'}),
-    Trip.create({name: 'Endor', location: 'Outer Rim Galaxy', imageUrl: './images/7a3de0b29c4cff3c550899f645038ff4.jpg', 
+    Trip.create({name: 'Endor', location: 'Outer Rim Galaxy', imageUrl: '/images/7a3de0b29c4cff3c550899f645038ff4.jpg', 
     price: '2500', description: 'Endor is known as the Forest Moon of Endor or the Sanctuary Moon was a small forested moon that orbited the Outer Rim planet of the same name and was the homeworld of the sentient Ewok and Yuzzum species.'}),
-    Trip.create({name: 'Metropolis', location: 'Unated States', imageUrl: './images/e96a0108b1ae7357a1a4038ed3ebae7f.jpg', 
+    Trip.create({name: 'Metropolis', location: 'Unated States', imageUrl: '/images/e96a0108b1ae7357a1a4038ed3ebae7f.jpg', 
     price: '2000', description: 'Metropolis is a fictional city appearing in American comic books published by DC Comics, best known as the home of Superman.'}),
-    Trip.create({name: 'Winterfell', location: 'The North', imageUrl: './images/8ae6981bf6ce6f296a00a6a8c618f5a7.jpg', 
+    Trip.create({name: 'Winterfell', location: 'The North', imageUrl: '/images/8ae6981bf6ce6f296a00a6a8c618f5a7.jpg', 
     price: '5000', description: 'Winterfell is the house of the North and the ancestral home of House Stark. It is a large stronghold extending across many acres of territory, defended by two layers of strong stone granite walls. The outer walls are 80 feet tall and the inner are 100 feet tall, with a wide moat located between them.'})
   ])
 
