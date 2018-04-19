18878f4905650a910cc85cde233367dbafc774d1
Melissa Bellah <melissabellah@gmail.com>
2018-04-19 08:21:29 -0400

    some code clean up and filter working

--- client/components/trips.js
+++ client/components/trips.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchAllTrips } from '../store/trips';
-// import Sidebar from './sidebar';
 
 class Trips extends Component {
 
   componentDidMount(){
-    console.log("getAllTrips in component did mount")
     this.props.getAllTrips();
   }
 
   trips(){
-    const { where=() => true } = this.props
+    const { where = () => true } = this.props
     return this.props.trips.filter(where)
   }
 
@@ -27,7 +25,7 @@ class Trips extends Component {
             return (
               <div className="trips"  key={trip.id}>
                 <img src={trip.imageUrl} />
-                <Link to={`/trips/${trip.id}`} className='trips'><h3>{trip.name}</h3></Link>
+                <Link to={`/trips/${trip.id}`} className="trips"><h3>{trip.name}</h3></Link>
                 <h5>{trip.location}</h5>
                 <h5>{`$${trip.price}`}</h5>
               </div>
