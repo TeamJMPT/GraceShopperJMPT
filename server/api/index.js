e15d7a7d9b9ed3e64c4967a37639e25c315730b0
Tasnuva Noor <noortasnuva@gmail.com>
2018-04-21 16:33:34 -0400

    cart api

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/trips', require('./trips'))
 router.use('/categories', require('./categories'))
-router.use('/orders', require('./orders'))
+router.use('/cart', require('./cart'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,9 +1,22 @@
-const router = require('express').Router()
-const {Trip, Order } = require('../db/models')
-module.exports = router
+// const router = require('express').Router()
+// const {Trip, Order } = require('../db/models')
+// module.exports = router
 
-// router.get('/', (req, res, next) => {
-//   Order.findAll({
+// // router.get('/', (req, res, next) => {
+// //   Order.findAll({
+// //     include: [{
+// //       model: Trip
+// //     }]
+// //   })
+// //     .then(orders => res.send(orders))
+// //     .catch(next);
+// // });
+
+// router.get('/:userId', (req, res, next) => {
+//   Order.findAll( {
+//     where: {
+//       userId: req.params.userId
+//     },
 //     include: [{
 //       model: Trip
 //     }]
@@ -12,29 +25,16 @@ module.exports = router
 //     .catch(next);
 // });
 
-router.get('/:userId', (req, res, next) => {
-  Order.findAll( {
-    where: {
-      userId: req.params.userId
-    },
-    include: [{
-      model: Trip
-    }]
-  })
-    .then(orders => res.send(orders))
-    .catch(next);
-});
+// router.post('/:userId', (req, res, next) => {
+//   Order.createOrFind({
+//     where: {id: req.params.orderId},
+//     include: [{model: Trip}]
+//   }, req.body)
+//     .then(newOrder => res.send(newOrder))
+//     .catch(next)
+// })
 
-router.post('/:userId', (req, res, next) => {
-  Order.createOrFind({
-    where: {id: req.params.orderId},
-    include: [{model: Trip}]
-  }, req.body)
-    .then(newOrder => res.send(newOrder))
-    .catch(next)
-})
-
-router.get('/', (req, res, next) => {
-  req.session.cart = []
-  console.log("here is session!", req.session)
-})
+// router.get('/', (req, res, next) => {
+//   req.session.cart = []
+//   console.log("here is session!", req.session)
+// })
