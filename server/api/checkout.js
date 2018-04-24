5d17303abac8439f9b3837dfcf37e7c53270462d
Janel Grant <janelgr5@gmail.com>
2018-04-24 14:37:03 -0400

    stripe checkout backend complete

---
+++ server/api/checkout.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+const { Cart, /*OrderHistory*/ } = require('../db/models')//Need order history model too
+// module.exports = router
+
+// STRIPE
+// These keys identify your account when you communicate with Stripe.
+const keyPublishable = 'pk_test_KAqpimzvub4Yo7MM026ah30C'
+const keySecret = 'sk_test_EDb5FTTy2icm6Qia71UkEbxq'
+// import stripe module; accepts a single parameter, the secret key associated with your account.
+const stripe = require('stripe')(keySecret)
+
+const postStripeCharge = res => (stripeErr, stripeRes) => {
+    if (stripeErr) res.status(500).send({error: stripeErr})
+    else res.status(200).send({success: stripeRes})
+}
+
+// A POST route (to the order history model) that receives the payment token ID and creates the charge
+// on a post request, that you are already doing with axios from your React frontend application, you will use the Stripe library to create a official Stripe payment.
+// The payment creation receives the incoming payload from your frontend application, all the credit card information and optional information, and a callback function that executes after the request to the Stripe API succeeds or fails.
+// Afterward, you can send back a response to your React frontend application.
+const checkout = router.post('/orderHistory', (req, res, next) => {
+    const charge = stripe.charges.create(req.body, postStripeCharge(res))
+    OrderHistory.create(charge)
+    .then(newCharge => res.status(201).json(newCharge))
+})
+
+module.exports = checkout;
--- server/api/index.js
+++ server/api/index.js
@@ -6,6 +6,7 @@ router.use('/trips', require('./trips'))
 router.use('/categories', require('./categories'))
 router.use('/orders', require('./orders'))
 router.use('/cart', require('./cart'))
+router.use('/checkout', require('./checkout'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
