9a87658d124e7583910caeb6aa4c056c95dd99f9
Melissa Bellah <melissabellah@gmail.com>
2018-04-24 20:49:37 -0400

    stripe checkout working!

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const { Cart, /*OrderHistory*/ } = require('../db/models')//Need order history model too
-// module.exports = router
+const { Cart } = require('../db/models')
+module.exports = router
 
 // STRIPE
 // These keys identify your account when you communicate with Stripe.
@@ -10,18 +10,29 @@ const keySecret = 'sk_test_EDb5FTTy2icm6Qia71UkEbxq'
 const stripe = require('stripe')(keySecret)
 
 const postStripeCharge = res => (stripeErr, stripeRes) => {
+    console.log("stripeErr!", stripeErr, "stripeRes", stripeRes)
     if (stripeErr) res.status(500).send({error: stripeErr})
     else res.status(200).send({success: stripeRes})
 }
+
+//Want to get this working
+// router.post('/payment', (req, res, next) => {
+//     Cart.update({status: 'complete'}, {
+//         where: {id: req.body.cartId},
+//         returning: true
+//     })
+//     .then(arr => res.send('Cart is now completed!'));
+// })
 
 // A POST route (to the order history model) that receives the payment token ID and creates the charge
 // on a post request, that you are already doing with axios from your React frontend application, you will use the Stripe library to create a official Stripe payment.
 // The payment creation receives the incoming payload from your frontend application, all the credit card information and optional information, and a callback function that executes after the request to the Stripe API succeeds or fails.
 // Afterward, you can send back a response to your React frontend application.
-const checkout = router.post('/orderHistory', (req, res, next) => {
-    const charge = stripe.charges.create(req.body, postStripeCharge(res))
-    OrderHistory.create(charge)
-    .then(newCharge => res.status(201).json(newCharge))
+router.get('/payment', (req, res, next) => {
+    res.send({message: 'Hello Stripe checkout server!', timestamp: new Date().toISOString})
 })
 
-module.exports = checkout;
+router.post('/payment', (req, res, next) => {
+    const request = { source: req.body.source, currency: req.body.currency, amount: req.body.amount}
+    stripe.charges.create(request, postStripeCharge(res))
+});
